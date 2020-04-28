70494f79d07bc5f4504d5bb553e58999a201e153
David VomLehn <vomlehn@texas.net>
2020-04-28 11:21:19 -0700

    Add code that to cause QEMU to exit when the program is done.
    See:
    	stackoverflow.com/questions/31990487/how-to-cleanly-exit-qemu-after-executing-bare-metal-program-without-user-interve

--- main.c
+++ main.c
@@ -1,15 +1,13 @@
 #include "uart_pl011.h"
 
-void my_exit(void)
-{
-	for (;;) {}
-}
+void my_exit(void);
 
 int main(int argc, char *argv[])
 {
 	struct UARTpl011 uart;
 
 	uart = uart_new(0x09000000);
-	uart_puts(uart, "Stub kernel started...\n");
+	uart_puts(uart, "Stub kernel started\n");
+	uart_puts(uart, "Stub kernel exiting\n");
 	my_exit();
 }
